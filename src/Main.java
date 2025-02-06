a538fa1bc44ec64cc30bd2768a7a88fb640322f3
João Gabriel <joaogudiluna@gmail.com>
2025-02-05 22:57:36 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,51 +2,60 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner escolhaUsuario = new Scanner(System.in);
         int escolha = 0;
 
         while (escolha != 5) {
-            System.out.println("\n----- Menu -----");
-            System.out.println("1 - Adição");
-            System.out.println("2 - Subtração");
-            System.out.println("3 - Multiplicação");
-            System.out.println("4 - Divisão");
-            System.out.println("5 - Saída");
-            System.out.print("\nEscolha uma opção: ");
-            escolha = escolhaUsuario.nextInt();
+        System.out.println("\n-----Menu-----");
+        System.out.println("1 - Adição");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplição");
+        System.out.println("4 - Divisão");
+        System.out.println("5 - Saida");
+        System.out.println("\nEscolha uma opção: ");
+        escolha = escolhaUsuario.nextInt();
 
-            if (escolha >= 1 && escolha <= 4) {
-                int numero1 = lerNumero("Digite um número: ", escolhaUsuario);
-                int numero2 = lerNumero("Digite outro número: ", escolhaUsuario);
-                int resultado = 0;
+        if (escolha == 1) {
+            System.out.println("Digite um numero: ");
+            int numeroAdicao = escolhaUsuario.nextInt();
+            System.out.println("Digite um numero: ");
+            int numeroAdicao2 = escolhaUsuario.nextInt();
+            int resultado = numeroAdicao + numeroAdicao2;
+            System.out.println("Resultado: " + resultado);
+        }
+        else if (escolha == 2) {
+            System.out.println("Digite um numero: ");
+            int numeroSubtracao = escolhaUsuario.nextInt();
+            System.out.println("Digite um numero: ");
+            int numeroSubtracao2 = escolhaUsuario.nextInt();
+            int resultado = numeroSubtracao - numeroSubtracao2;
+            System.out.println("Resultado: " + resultado);
 
-                switch (escolha) {
-                    case 1 -> resultado = numero1 + numero2;
-                    case 2 -> resultado = numero1 - numero2;
-                    case 3 -> resultado = numero1 * numero2;
-                    case 4 -> {
-                        if (numero2 == 0) {
-                            System.out.println("Erro: Não é possível dividir por zero!");
-                            continue;
-                        }
-                        resultado = numero1 / numero2;
-                    }
-                }
+        }
+        else if (escolha == 3) {
+            System.out.println("Digite um numero: ");
+            int numeroMultipliacao = escolhaUsuario.nextInt();
+            System.out.println("Digite um numero: ");
+            int numeroMultipliacao2 = escolhaUsuario.nextInt();
+            int resultado = numeroMultipliacao * numeroMultipliacao2;
+            System.out.println("Resultado: " + resultado);
 
-                System.out.println("Resultado: " + resultado);
-            } else if (escolha == 5) {
-                System.out.println("Saindo...");
-                break;
-            } else {
-                System.out.println("Opção inválida! Tente novamente.");
-            }
+        }
+        else if (escolha == 4) {
+            System.out.println("Digite um numero: ");
+            int numeroDivisao = escolhaUsuario.nextInt();
+            System.out.println("Digite um numero: ");
+            int numeroDivisao2 = escolhaUsuario.nextInt();
+            int resultado = numeroDivisao / numeroDivisao2;
+            System.out.println("Resultado: " + resultado);
+
+        }
+        else if (escolha == 5) {
+            System.out.println("Saindo...");
+            break;
+        }
         }
 
-        escolhaUsuario.close();
-    }
-
-    private static int lerNumero(String mensagem, Scanner scanner) {
-        System.out.print(mensagem);
-        return scanner.nextInt();
     }
 }
